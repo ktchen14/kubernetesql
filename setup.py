a213a8171e8433909dea5f00e6a25c9235687b83
Ashuka Xue, Ben Christel and Kaiting Chen <pair+axue+bchristel+kaiting@pivotal.io>
2018-06-27 10:06:54 -0700

    Skeleton of Python module with test

---
+++ setup.py
@@ -0,0 +1,10 @@
+import subprocess
+from setuptools import setup, find_packages, Extension
+
+setup(
+  name='kubernetesql',
+  version='0.0.1',
+  author='The kubernetesql hack team',
+  license='Postgresql',
+  packages=['kubernetesql']
+)
