e8281d5d7a8bec40ac91170d0757f5c50d77d0f1
Melanie Plageman <melanieplageman@gmail.com>
2018-06-28 08:36:55 -0700

    update demo query

--- demo/demo_query.sql
+++ demo/demo_query.sql
@@ -6,11 +6,17 @@ ON pods.node_name = nodes.name;
 SELECT name, desired, current_num, available, age FROM deployments WHERE name = 'nginx-deployment';
 UPDATE deployments SET desired = 5 WHERE name = 'nginx-deployment';
 SELECT name, desired, current_num, available, age FROM deployments WHERE name = 'nginx-deployment';
+SELECT name, deployment_name, ready, status, age FROM pods WHERE deployment_name = 'nginx-deployment';
 
 -- get a list of employee emails where the employee is the owner of a deployment impacted by the outage
+
+SELECT * FROM employee;
+
+SELECT * FROM cves;
 
 SELECT DISTINCT deployments.name as deployment_name, employee.name, email FROM employee
 INNER JOIN deployments ON employee.user_id::TEXT = annotations->>'user'
 INNER JOIN pods ON pods.deployment_name = deployments.name
 INNER JOIN cves ON cves.docker_image = ANY(pods.image_names);
 
+-- Containers in pods in our cluster
